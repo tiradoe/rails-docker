05ecad8374bc59753c026196d602daa80f73ff6b
Edward Tirado Jr <ed@etdevelopment.net>
2021-03-09 20:18:16 -0700

    added react-rails and postInstall script

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,9 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import "stylesheets/application"
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
---
+++ postInstall.sh
@@ -0,0 +1,4 @@
+./rails.sh rails webpacker:install
+./rails.sh rails webpacker:install:react
+./rails.sh rails tailwindcss:install
+./rails.sh rails generate react:install
